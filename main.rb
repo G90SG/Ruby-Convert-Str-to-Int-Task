9a6219627d86656164faca52051a3d3e10c3bae7
Gemma Dingwall <gemma9261@hotmail.com>
2021-01-18 16:18:07 +0000

    Initial commit

---
+++ main.rb
@@ -0,0 +1,11 @@
+# Create variable
+string = "Gemma loves pizza"
+# finding out the string size
+length = string.size
+# converting the string size to int
+length = length.to_i
+# printing the length
+puts "The length is #{length}."
+# working out length to the power of length
+power = length ** length
+puts "The length to the power of the length is \n#{power}"
